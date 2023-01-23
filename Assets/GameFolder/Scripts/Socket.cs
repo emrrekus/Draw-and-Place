07c62441e79b674ccbcbbb1da7782c3b23f29a2c
Emre <112270755+emrrekus@users.noreply.github.com>
2023-01-23 20:09:15 +0300

    aa

--- Assets/GameFolder/Scripts/Socket.cs
+++ Assets/GameFolder/Scripts/Socket.cs
@@ -9,6 +9,10 @@ public class Socket : MonoBehaviour
     public bool Settle;
     [SerializeField] private int LineIndex;
     [SerializeField] private string _socketColor;
+
+    private bool _Nest;
+    private Vector2 _NestPosition;
+    [SerializeField] private GameObject _FinishNest;
     private void Update()
     {
         if (Settle)
@@ -20,6 +24,16 @@ public class Socket : MonoBehaviour
             
 
         }
+
+        if (_Nest)
+        {
+            if (Vector2.Distance(transform.position, _NestPosition) > .1f)
+                transform.position = Vector2.Lerp(transform.position, _NestPosition, .2f);
+            else
+                transform.position = _NestPosition;
+
+            _Nest = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -27,6 +41,8 @@ public class Socket : MonoBehaviour
         if (col.CompareTag(_socketColor))
         {
             Debug.Log("Doğru" + _socketColor);
+            _Nest = true;
+            _NestPosition = _FinishNest.transform.position;
         }
         else if (col.CompareTag("Socket"))
         {
@@ -35,8 +51,11 @@ public class Socket : MonoBehaviour
         }
         else
         {
-            if(! col.CompareTag(General._GameManager._LineCreates[LineIndex]._Tag))
-            Debug.Log("Game Over"+col.gameObject.name);
+            if (!col.CompareTag(General._GameManager._LineCreates[LineIndex]._Tag))
+            {
+                Settle = false;
+            }
+            
         }
     }
 }
